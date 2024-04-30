f7ba78a6b1e4c88c743545a253fbaff8b259f1ec
奕辰 <116084043+code-517@users.noreply.github.com>
2024-04-30 16:02:03 +0000

    1

--- 20240430/11022345all.py
+++ 20240430/11022345all.py
@@ -17,4 +17,5 @@ print(df2[['Time','gate', 'Value2']].head())
 # 將 df1 和 df2 進行合併，並只保留 'Value2' 欄位
 df1 = pd.merge(df1, df2[['Time','gate', 'Value2']], how='left', left_on=['time', 'gate'], right_on=['Time', 'gate'])
 df1 = df1.drop(columns=['Time'])
+df1 = df1.rename(columns={'Value2': '31小客車速度'})
 df1.to_csv('/workspaces/cycu_ai2024/20240430/ALL.csv', index=False)
