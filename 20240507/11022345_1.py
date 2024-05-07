e1b033e660d728c49c745e6182e2717bd3f2378e
奕辰 <116084043+code-517@users.noreply.github.com>
2024-05-07 12:04:45 +0000

    1

--- 20240507/11022345_1.py
+++ 20240507/11022345_1.py
@@ -5,3 +5,4 @@ df = pd.read_csv('/workspaces/cycu_ai2024/20240430/ALL.csv')
 #把5-8的欄位都刪掉並輸出一個新的csv檔
 df = df.drop(df.columns[4:8], axis=1)
 df.to_csv('/workspaces/cycu_ai2024/20240507/11022345_1.csv', index=False)
+#ploply
